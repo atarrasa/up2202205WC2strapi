c13fbd57fe133a8ac59c2d88e0d206d473126b2b
Alvaro <alvaro_gonzalez_tsapp1ob2021@cev.com>
2024-05-06 19:40:10 +0200

    pipeline issue

--- config/admin.js
+++ config/admin.js
@@ -1,9 +1,9 @@
 module.exports = ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET'),
+    secret: env('77b2c87dbab4e1697bec244226fbd1b3'),
   },
   apiToken: {
-    salt: env('API_TOKEN_SALT'),
+    salt: env('d9b0df66ff97a666027e665707b4e3e7'),
   },
   transfer: {
     token: {
