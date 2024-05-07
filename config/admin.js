7a351ec0bee3b16a70dcfc1f65f9932d9305dddd
atarrasa <147190310+atarrasa@users.noreply.github.com>
2024-05-07 10:15:17 +0200

    Update admin.js

--- config/admin.js
+++ config/admin.js
@@ -5,5 +5,11 @@ module.exports = ({ env }) => ({
   auth: {
     secret: env('ADMIN_JWT_SECRET', '77b2c87dbab4e1697bec244226fbd1b3'),
   },
+    transfer: {
+    token: {
+    salt: env('TRANSFER_TOKEN_SALT', 'd9b0df66ff97a666027e665707b4e3e8'),
+    
+      },
+  },
     url:"/safeadmin"
 });
